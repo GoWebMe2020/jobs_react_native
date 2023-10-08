13ae086886c2d6721528b9fc5924a4c9663d91c7
JP Ferreira <jpferreira1982@gmail.com>
2023-10-08 17:28:02 +1100

    Adding the nearby jobs to fetch and display in a list. Adding some styling

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 // import { RAPID_API_KEY } from '@env'
-import { isLoading } from 'expo-font';
+// import { isLoading } from 'expo-font';
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   // const rapidApiKey = RAPID_API_KEY;
 
@@ -20,17 +20,17 @@ const useFetch = (endpoint, query) => {
   };
 
   const fetchData = async () => {
-    setLoading(true);
+    setIsLoading(true);
 
     try {
       const response = await axios.request(options);
       setData(response.data.data);
-      setLoading(false);
+      setIsLoading(false);
     } catch (error) {
       setError(error);
       alert('There is an error.');
     } finally {
-      setLoading(false);
+      setIsLoading(false);
     }
   }
 
@@ -43,7 +43,7 @@ const useFetch = (endpoint, query) => {
     fetchData();
   }
 
-  return { data, loading, error, refetch };
+  return { data, isLoading, error, refetch };
 }
 
 export default useFetch;
